a9fb28fc3d0bf0d4ce17df9b813f2d6a1c3d5abf
Tom Dunkle <thomas.dunkle@gmail.com>
2025-07-06 12:36:44 -0500

    Make comment in download_large_files.ps1 even more concise.

--- download_large_files.ps1
+++ download_large_files.ps1
@@ -1,6 +1,6 @@
 # Two files in the tomDunkleCom repository are over 100MB, which means that they cannot be hosted
-#  directly in GitHub. This script downloads those files directly from my Google Drive, where they
-#  are publicly shared. This script requires gdown to run. gdown can be installed through pip:
+#  in GitHub. This script downloads those files directly from my Google Drive, where they are
+#  publicly shared. This script requires gdown to run. gdown can be installed through pip:
 #  pip install gdown
 
 gdown https://drive.google.com/uc?id=1A4yYhlgMtY5yV-Mxbdk7jIWNrv8OyZF3 -O .\deersim\audio\startlinglyYOURFEARISREALinanothertime.wav
