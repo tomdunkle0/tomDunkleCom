56305ade8bff71033b02a8dedde7a7f289e26edf
Tom Dunkle <thomas.dunkle@gmail.com>
2025-07-06 12:30:46 -0500

    Update download_large_files.ps1 to also download startlinglyYOURFEARISREALinanothertime.wav

--- README.md
+++ README.md
@@ -2,12 +2,3 @@ tomDunkleCom
 ==============
 
 This is the public repository for the content hosted at www.tomdunkle.com.
-
-
-
-Known Issues
------
-
-The following are issues I've identified while migrating the project into this git repository:
-
- * All of the audio files in deersim/audio are in .wav format, and consequently one of the files (startlinglyYOURFEARISREALinanothertime.wav) is over 100MB, making it too large to host on github without using large file storage (LFS).
--- download_large_files.ps1
+++ download_large_files.ps1
@@ -1,6 +1,7 @@
-# A select few certain files in the tomDunkleCom repository are over 100MB, which means that they
-#  cannot be hosted directly in git. This script downloads those files directly from my Google
-#  Drive account, where they are publicly shared. This script requires gdown to run. gdown can be
-#  installed through pip with: pip install gdown
+# Two files in the tomDunkleCom repository are over 100MB, which means that they cannot be hosted
+#  directly in git. This script downloads those files directly from my Google Drive, where they are
+#  publicly shared. This script requires gdown to run. gdown can be installed through pip:
+#  pip install gdown
 
+gdown https://drive.google.com/uc?id=1A4yYhlgMtY5yV-Mxbdk7jIWNrv8OyZF3 -O .\deersim\audio\startlinglyYOURFEARISREALinanothertime.wav
 gdown https://drive.google.com/uc?id=1Hj3G0y0QNlfq-lRnH59wOJtGjl_ANwIM -O .\high_points\nv\walking_up_hills_ep_01.mp4
