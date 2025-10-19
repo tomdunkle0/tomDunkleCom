896b368b9dd54b022131037882d7a7e9ac3708bc
Tom Dunkle <135174836+tomdunkle0@users.noreply.github.com>
2025-10-18 19:10:21 -0500

    [high_points] Fix year strings displaying squished together and indistinguishable from one another. (#40)
    * [high_points] Use a web-hosted font family (Oswald) for the year strings so that they appear uniform across platforms.
    
    * Organize elements in the HTML head.
    
    * Define populateHTMLHead().
    
    * Add the word Element to each link and title element in html.test.js
    
    * Improve const names in test for page title.
    
    * Fix alphabetical ordering of tests.
    
    * Update the revision date on the website's top-level index.html

---
+++ high_points/tests/html.test.js
@@ -0,0 +1,56 @@
+const fs = require('fs');
+const path = require('path');
+
+// Function includes:
+const mapFunctions = require('./environment/mapFunctions');
+const isUnpopulated = mapFunctions.isUnpopulated;
+
+// Miscellaneous constants:
+const kElementTypeLink = "link";
+
+// HTML Element URLs:
+const cssStyleSheetUrl = "[href=\"high_points/high_points_style.css\"]";
+const googleAPIsUrl = "[href=\"https://fonts.googleapis.com\"]";
+const gStaticUrl = "[href=\"https://fonts.gstatic.com\"]";
+const oswaldFontUrl = "[href=\"https://fonts.googleapis.com/css2?family=Oswald&display=swap\"]";
+
+beforeAll(() => {
+    populateHTMLHead();
+}); // beforeAll()
+
+test(`the HTML head contains preconnect links for the year string font`, () => {
+    const relationship = "[rel=\"preconnect\"]";
+    const googleAPIsSelector = kElementTypeLink + relationship + googleAPIsUrl;
+    const googleAPIsLinkElement = document.head.querySelector(googleAPIsSelector);
+    const crossOriginAttribute = "[crossorigin]";
+    const gStaticSelector = kElementTypeLink + relationship + gStaticUrl + crossOriginAttribute;
+    const gStaticLinkElement = document.head.querySelector(gStaticSelector);
+    expect(isUnpopulated(googleAPIsLinkElement)).toBe(false);
+    expect(isUnpopulated(gStaticLinkElement)).toBe(false);
+}); /* the HTML head contains preconnect links for the year string font */
+
+test(`the HTML head contains the page title`, () => {
+    const titleElementSelector = "title";
+    const titleElement = document.head.querySelector(titleElementSelector);
+    const pageTitle = titleElement.textContent;
+    const expectedPageTitle = "United States High Points";
+    expect(pageTitle).toBe(expectedPageTitle);
+}); /* the HTML head contains the page title */
+
+test(`the HTML head contains the stylesheets necessary to render the user interface`, () => {
+    const relationship = "[rel=\"stylesheet\"]";
+    const cssStyleSheetSelector = kElementTypeLink + relationship + cssStyleSheetUrl;
+    const cssStyleSheetLinkElement = document.head.querySelector(cssStyleSheetSelector);
+    const oswaldFontSelector = kElementTypeLink + relationship + oswaldFontUrl;
+    const oswaldFontLinkElement = document.head.querySelector(oswaldFontSelector);
+    expect(isUnpopulated(cssStyleSheetLinkElement)).toBe(false);
+    expect(isUnpopulated(oswaldFontLinkElement)).toBe(false);
+}); /* the HTML head contains the stylesheets necessary to render the user interface */
+
+function populateHTMLHead()
+{
+    const htmlFileRelativePath = "../../high_points.html";
+    const htmlFileAbsolutePath = path.join(__dirname, htmlFileRelativePath);
+    const utf8EncodingOption = "utf8";
+    document.documentElement.innerHTML = fs.readFileSync(htmlFileAbsolutePath, utf8EncodingOption);
+}
