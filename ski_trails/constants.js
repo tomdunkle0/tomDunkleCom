3e5544a2457175b0d313fef400fcfc86f0a50e7a
Tom Dunkle <thomas.dunkle@gmail.com>
2026-08-20 20:04:26 -0500

    Add prototype of Winter Park Ski Trails app to www.tomdunkle.com

---
+++ ski_trails/constants.js
@@ -0,0 +1,24 @@
+// Polyline stroke colors:
+const kPolylineStrokeColorBlack  = "black";
+const kPolylineStrokeColorBlue   = "blue";
+const kPolylineStrokeColorGreen  = "green";
+const kPolylineStrokeColorPurple = "purple";
+const kPolylineStrokeColorRed    = "red";
+
+// Trail difficulties:
+const kTrailDifficultyAdvancedIntermediate = "Advanced Intermediate";
+const kTrailDifficultyEasy                 = "Easy";
+const kTrailDifficultyExpert               = "Expert";
+const kTrailDifficultyExtremeTerrain       = "Extreme Terrain";
+const kTrailDifficultyIntermediate         = "Intermediate";
+
+// Trail statuses:
+//  The response from mtnpowder has been found to have various formats for the 'closed' status.
+const kTrailStatusClosed1 = "closed";
+const kTrailStatusClosed2 = "Closed";
+const kTrailStatusClosed3 = "Closed for Season";
+const kTrailStatusOpen    = "Open";
+
+// Miscellaneous constants:
+const kAttributeNameStroke = "stroke";
+const kResortDataSourceUrl = "https://v4.mtnfeed.com/resorts/winter-park.json";
---
+++ ski_trails/mapFunctions.js
@@ -0,0 +1,94 @@
+/*--------------------------------------------------------------------------*\
+ | File: mapFunctions.js                                                    |
+ | Purpose: Defines functions related to the United States High Points map. |
+\*--------------------------------------------------------------------------*/
+
+var gDisplayCurrentStatus = true;
+var gTrailData = null;
+
+function assignTrailColors()
+{
+    const mtnAreas = gTrailData.MountainAreas;
+    for (var areaIndex = 0; areaIndex < mtnAreas.length; areaIndex++)
+    {
+        const mtnArea = mtnAreas[areaIndex];
+        for (var trailIndex = 0; trailIndex < mtnArea.Trails.length; trailIndex++)
+        {
+            const trail = mtnArea.Trails[trailIndex];
+            const actualTrail = document.getElementById(trail.Name);
+            if (actualTrail !== null)
+            {
+                actualTrail.setAttribute("stroke-width", "0.3px");
+                if ((trail.Status === kTrailStatusOpen) || (!gDisplayCurrentStatus))
+                {
+                    switch (trail.Difficulty)
+                    {
+                        case kTrailDifficultyAdvancedIntermediate:
+                            actualTrail.setAttribute(kAttributeNameStroke, kPolylineStrokeColorPurple);
+                            break;
+                        case kTrailDifficultyEasy:
+                            actualTrail.setAttribute(kAttributeNameStroke, kPolylineStrokeColorGreen);
+                            break;
+                        case kTrailDifficultyExpert:
+                        case kTrailDifficultyExtremeTerrain:
+                            actualTrail.setAttribute(kAttributeNameStroke, kPolylineStrokeColorBlack);
+                            break;
+                        case kTrailDifficultyIntermediate:
+                            actualTrail.setAttribute(kAttributeNameStroke, kPolylineStrokeColorBlue);
+                            break;
+                    }
+                }
+
+                if (gDisplayCurrentStatus)
+                {
+                    const trailStatus = trail.Status;
+                    if ((trailStatus === kTrailStatusClosed1)
+                     || (trailStatus === kTrailStatusClosed2)
+                     || (trailStatus === kTrailStatusClosed3)
+                    )
+                    {
+                        actualTrail.setAttribute(kAttributeNameStroke, kPolylineStrokeColorRed);
+                    }
+                }
+            }
+        }
+    }
+} // assignTrailColors()
+
+function getJsonFromResponse(response)
+{
+    if (!response.ok)
+    {
+        throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    return response.json();
+} // getJsonFromResponse()
+
+function assignTrailColorsAsync(jsonData)
+{
+    var bearerToken = jsonData.bearerToken;
+    const url = `https://mtnpowder.com/feed/v3.json?bearer_token=${bearerToken}&resortId=5`
+    fetch(url)
+        .then(getJsonFromResponse)
+        .then(jsonData => {
+        gTrailData = jsonData;
+        assignTrailColors();
+    });
+} // assignTrailColorsAsync()
+
+function onClickMap()
+{
+    gDisplayCurrentStatus = !gDisplayCurrentStatus;
+    assignTrailColors();
+} // onClickMap()
+
+function onPageLoad()
+{
+    document.body.innerHTML = getMapContent();
+    fetch(kResortDataSourceUrl)
+        .then(getJsonFromResponse)
+        .then(assignTrailColorsAsync)
+        .catch(error => {
+        console.error("Error fetching JSON:", error);
+    });
+} // onPageLoad()
